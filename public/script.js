74ff046efdac3106a96e71486364205d789c65fb
Melike Bozoğlan <m.sultanbozoglan@gmail.com>
2024-10-28 18:10:39 +0300

    finished

--- public/script.js
+++ public/script.js
@@ -1,1 +1,31 @@
+const socket = io.connect('http://localhost:3000')
 
+const sender = document.getElementById('sender')
+const message = document.getElementById('message')
+const submitBtn = document.getElementById('button')
+const output = document.getElementById('output')
+const feedback = document.getElementById('feedback')
+
+submitBtn.addEventListener('click', () => {
+    socket.emit('chat', {
+        sender: sender.value,
+        message: message.value,
+    })
+})
+
+socket.on('chat', data => {
+    //feedback sıfırla 
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.sender + ': </strong>' + data.message + '</p>'
+    // message sıfırla
+    message.value = '';
+})
+
+// keypress olunca 
+message.addEventListener('keypress', () => {
+    socket.emit('typing', sender.value)
+})
+
+socket.on('typing', data => {
+    feedback.innerHTML = '<p>' + data + ' typing...</p>'
+})
